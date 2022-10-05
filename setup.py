64bf6206c139788dd3e62601433c5cb6788cb0f4
Sylensky <admin@medieval-realm.net>
2022-10-05 11:40:34 +0200

    prepare v0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="poe-hat-b",
-    version="0.1.0",
+    version="0.1.1",
     author="Sylensky",
     author_email="admin@medieval-realm.net",
     description="Waveshare POE Hat (B) for RPi4",
